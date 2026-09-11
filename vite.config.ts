fd02f88915adfb75ca828fb1ad17f7503bc2754b
WhiteLittleFox <Rantelokmine@gmail.com>
2026-09-11 22:52:44 +0500

    'a'

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'My App',
+				short_name: 'App',
+				theme_color: '#ffffff'
+			},
+			workbox: {
+				navigateFallback: '/offline.html',
+				runtimeCaching: [
+				{
+					urlPattern: /^https:\/\/.*/,
+					handler: 'NetworkFirst',
+					options: { cacheName: 'pages' }
+				}]
+			}
+		})
+	],
 })

a9ad985ba2b0493ab9ba2ac6df0fadde7c15d6c3
WhiteLittleFox <Rantelokmine@gmail.com>
2026-09-15 00:57:25 +0500

    unpwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	plugins: [
 		react(),
 		tailwindcss(),
-		VitePWA({
-			workbox: {
-				navigateFallback: '/offline.html',
-				navigateFallbackAllowlist: [/^\/.*$/],
-				globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-				cleanupOutdatedCaches: true,
-				runtimeCaching: [
-					{
-						urlPattern: /\/[^.]*$/,
-						handler: 'NetworkFirst',
-						options: {
-							cacheName: 'navigation',
-							networkTimeoutSeconds: 3
-						}
-					}
-				]
-			}
-		})
 	],
 })

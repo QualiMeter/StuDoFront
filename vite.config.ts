a29a977b87e62a7487fe5b3a33eb6ad260d7acb8
WhiteLittleFox <Rantelokmine@gmail.com>
2026-09-14 23:44:49 +0500

    pub2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 		tailwindcss(),
 		VitePWA({
 			workbox: {
-				navigateFallback: '/public/offline.html',
+				navigateFallback: '/offline.html',
 				navigateFallbackDenylist: [/^\/api\//],
 				runtimeCaching: [
 					{
